08f69b467c1c2ee773e8e22a80248a82be2c614b
cajsoft <spam@cajsoft.co.uk>
2023-03-15 14:07:16 +0000

    Add error checking on Sunrise/Sunset

--- solarpower.py
+++ solarpower.py
@@ -21,18 +21,22 @@ logging.basicConfig(level=logging.DEBUG,
 
 def sunrisesunset(sun):
 	# Set Location for Sunrise/Sunset
+
 	params = {"lat":55.614719, "lng":-4.498792, "date":date.today()}
 
 	f = r"https://api.sunrise-sunset.org/json?"
-	a = requests.get(f, params=params)
-	a = json.loads(a.text)
-	a = a["results"]
-	if sun == "Rise":
-		sun = datetime.datetime.strptime(a["sunrise"], '%I:%M:%S %p')
-	else:
-		sun = datetime.datetime.strptime(a["sunset"], '%I:%M:%S %p')
-	sun = sun.strftime('%H')
-	return (int(sun))
+	try:
+		a = requests.get(f, params=params)
+		a = json.loads(a.text)
+		a = a["results"]
+		if sun == "Rise":
+			sun = datetime.datetime.strptime(a["sunrise"], '%I:%M:%S %p')
+		else:
+			sun = datetime.datetime.strptime(a["sunset"], '%I:%M:%S %p')
+		sun = sun.strftime('%H')
+		return (int(sun))
+	except requests.exceptions.HTTPError as err:
+		return 0
 
 def fiveminutesPassed(oldminute):
 	currentminute = time.gmtime()[4]
